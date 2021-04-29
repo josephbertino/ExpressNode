539ba64a37d6b1e8e11c1c11b1798d1a9350e542
Joe Bertino <joseph.bertino@gmail.com>
2021-04-28 20:31:09 -0600

    getting things setup for Heroku

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(errorRouter.notFoundError);
 app.use(errorRouter.globalError);
 
 /* Start the app and listen on port 3000 */
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('Server listening on port 3000!');
 });
